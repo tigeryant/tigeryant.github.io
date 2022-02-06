6ccc3386544f13844f1f4e581b25a4a50b887406
John Davies <jd.other@hotmail.com>
2022-02-06 19:42:38 +0000

    added responsive grid layout to 'more projects' section

--- client/src/components/projects-children/MoreProjects.js
+++ client/src/components/projects-children/MoreProjects.js
@@ -2,7 +2,26 @@ function MoreProjects() {
   return (
     <section id="more-projects">
       <h5>More projects</h5>
-      <p>Two or three noteworthy projects</p>
+      <div className="row">
+        <div className="col-md-6 col-lg-4">
+          <p className="project-card p-5">Project 1</p>
+        </div>
+        <div className="col-md-6 col-lg-4">
+          <p className="project-card p-5">Project 2</p>
+        </div>
+        <div className="col-md-6 col-lg-4">
+          <p className="project-card p-5">Project 3</p>
+        </div>
+        <div className="col-md-6 col-lg-4">
+          <p className="project-card p-5">Project 4</p>
+        </div>
+        <div className="col-md-6 col-lg-4">
+          <p className="project-card p-5">Project 5</p>
+        </div>
+        <div className="col-md-6 col-lg-4">
+          <p className="project-card p-5">Project 6</p>
+        </div>
+      </div>
     </section>
   )
 }
