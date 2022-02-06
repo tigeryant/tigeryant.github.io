8c0c0616ad4bee78ddf95eeed60ae029ed220a38
John Davies <jd.other@hotmail.com>
2022-02-06 00:49:13 +0000

    added initial layout for featured project one

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,7 @@
-function Footer(){
+function Footer() {
   return (
-    // fluid container
-    // <h3>Footer</h3>
+  // fluid container
+  // <h3>Footer</h3>
 
     <div className="bg-dark text-light p-5" >
       <div className="container-xxl" >
@@ -20,7 +20,7 @@ function Footer(){
           </div>
           <div className="col">
             <div className="p-5 footer-element text-muted">
-              <span dangerouslySetInnerHTML={{ "__html": "&copy;" }} />2022 John Davies
+              <span dangerouslySetInnerHTML={{ '__html': '&copy;' }} />2022 John Davies
             </div>
           </div>
         </div>
--- client/src/components/projects-children/FeaturedOne.js
+++ client/src/components/projects-children/FeaturedOne.js
@@ -2,7 +2,19 @@ function FeaturedOne() {
   return (
     <section id="featured-one">
       <div className="featured-container">
-        <h5>Featured project one</h5>
+        <div className="featured-image">
+          {/* image */}
+          <p>featured image</p>
+        </div>
+        <div className="featured-info">
+          <p>Featured project</p>
+          <h5>Project title</h5>
+          <p className="info-card">Project card</p>
+          <p>tech</p>
+          <p>links to project and repo</p>
+        </div>
+
+        {/* <h5>Featured project one</h5> */}
       </div>
     </section>
   )
