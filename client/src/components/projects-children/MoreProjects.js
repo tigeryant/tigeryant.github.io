f156c6d4bec78e779ffff4c61475de5aa226a080
John Davies <jd.other@hotmail.com>
2022-02-09 14:50:51 +0000

    changed project display from hardcoding to mapping

--- client/src/components/projects-children/MoreProjects.js
+++ client/src/components/projects-children/MoreProjects.js
@@ -1,9 +1,15 @@
 function MoreProjects() {
+  // declare and array of objects - one object for each of the projects. Then map through them in the JSX
+
+  const projects = [
+    { title: 'A Star Pathfinding Algorithm', description: 'Interactive visualisation of the A star pathfinding algorithm.', technologies: ['Python', 'Pygame'], links: ['https://github.com/tigeryant/a-star-pathfinding-algorithm'] }
+  ]
+
   return (
     <section id="more-projects">
       <h3 className="mt-5">More projects</h3>
       <div className="row">
-        <div className="col-md-6 col-lg-4">
+        {/* <div className="col-md-6 col-lg-4">
           <div className="project-card p-3">
             <h4>A Star Pathfinding Algorithm</h4>
             <p className="p1 light-text">Interactive visualisation of the A star pathfinding algorithm.</p>
@@ -13,8 +19,40 @@ function MoreProjects() {
             </ul>
             <a href="https://github.com/tigeryant/a-star-pathfinding-algorithm" className="icon-link"><i className="bi bi-github" target="_blank" rel="noreferrer"></i></a>
           </div>
-        </div>
-        <div className="col-md-6 col-lg-4">
+        </div> */}
+        {projects.map((project, index) => {
+          return (
+            <div key={index} className="col-md-6 col-lg-4">
+              <div className="project-card p-3">
+                <h4>{project.title}</h4>
+                <p className="p1 light-text">{project.description}</p>
+                <ul className="p1 light-text project-tech">
+                  {project.technologies.map((tech, index) => {
+                    if (index === (project.technologies.length - 1)) {
+                      return (
+                        <li key={index} style={{ display: 'inline' }}>{tech}</li>
+                      )
+                    }
+
+                    return (
+                      <li key={index} style={{ display: 'inline' }}>{tech} &#8226; </li>
+                    )
+                  })}
+                </ul>
+                <div className="project-links-container">
+                  {project.links.map((link, index) => {
+                    const iconClass = (index === (project.links.length - 1) ? 'bi-github' :  'bi-box-arrow-up-right')
+                    return (
+                      <a key={index} href={link} className="icon-link"><i className={`bi ${iconClass}`} target="_blank" rel="noreferrer"></i></a>
+                    )
+                  })}
+                </div>
+              </div>
+            </div>
+          )
+        })}
+
+        {/* <div className="col-md-6 col-lg-4">
           <div className="project-card p-3">
             <h3>Order Matching Engine</h3>
             <p className="p1"> Price/time priority order matching engine with graphical user interface.</p>
@@ -76,7 +114,7 @@ function MoreProjects() {
             <a href="https://github.com/tigeryant/sei-project-one" className="icon-link"><i className="bi bi-github" target="_blank" rel="noreferrer"></i></a>
             <a href="https://tigeryant.github.io/sei-project-one/" className="icon-link"><i className="bi bi-box-arrow-up-right" target="_blank" rel="noreferrer"></i></a>
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   )
