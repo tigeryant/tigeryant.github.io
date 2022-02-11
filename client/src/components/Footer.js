1438ee4127c0ffd2bc6ead63ea4ad3643123e63d
John Davies <jd.other@hotmail.com>
2022-02-11 07:38:36 +0000

    updated section layouts

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,16 +3,10 @@ function Footer() {
   // fluid container
   // <h3>Footer</h3>
 
-    <div className="bg-dark text-light p-5" >
-      <div className="container-xxl" >
-        <div className="row" >
-          <div className="col">
-            <div className="p-5 footer-element text-muted">Built by John Davies, inspired by Brittany Chiang
-            </div>
-          </div>
-        </div>
+    <section id="footer" className="pb-5">
+      <div className="footer-element text-muted">Built by John Davies, inspired by Brittany Chiang.
       </div>
-    </div>
+    </section>
   )
 }
 
