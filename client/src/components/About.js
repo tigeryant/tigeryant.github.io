2bd4e8ff46500a906c731658bb1b7553c79f39bf
John Davies <jd.other@hotmail.com>
2022-02-11 05:56:22 +0000

    updated side bar and about section

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,16 +1,21 @@
+import portrait from '../assets/portrait.JPG'
+
 function About() {
   return (
     <section className="p-5" id="about">
       <div className="about-container row">
-        <div className="about-text col-8">
-          <p className="p2" style={{ display: 'inline' }}>01. </p>
-          <h2 style={{ display: 'inline' }}>About Me</h2>
+        <div className="about-text col-7">
+          <div className="pb-4">
+            <p className="p2" style={{ display: 'inline' }}>01. </p>
+            <h2 style={{ display: 'inline' }}>About Me</h2>
+          </div>
           <p className="p1">
             Hi! My name is John. I am a dynamic and voracious learner who is always looking for a subject to learn or a project to build. Having a naturally logical, abstract and analytical way of reasoning is what drew me to software engineering. During a full time software engineering course at General Assembly, I took to web development with brisk readiness. I&apos;m now actively looking for an opportunity to prove myself as a hardworking and enthusiastic member of a development team, who is eager to develop their skills at every chance. More specifically, I&apos;m looking for a role as a full stack, back end or front end engineer.  Some of my interests include mathematics, cryptography, Bitcoin and finance.
           </p>
         </div>
+        <div className="col-1"></div>
         <div className="about-image-container col-4">
-          <div className="about-image"></div>
+          <img src={portrait} className="about-image" />
         </div>
       </div>
     </section>
