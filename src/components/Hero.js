429594d47f801ad333465346a01bceef81e8ed73
John Davies <jd.other@hotmail.com>
2022-02-22 11:44:52 +0000

    updated hero content

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ function Hero() {
     <section className="p-5" id="hero">
       <p className="p2 mono-font">Hi, my name is</p>
       <h1>John Davies</h1>
-      <p className="p1">I&apos;m a junior software engineer looking for new opportunities in web development.</p>
+      <p className="p1">I&apos;m a junior software engineer looking for new opportunities in backend development.</p>
     </section>
   )
 }
