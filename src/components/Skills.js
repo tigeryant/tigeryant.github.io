94560482e474a1b2649ceaf14c964268552bb7ed
John Davies <jd.other@hotmail.com>
2022-11-06 20:36:05 +1300

    mobile responsiveness updates

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -278,13 +278,13 @@ function Skills() {
       <div className="flags-container my-5">
         <div className="skill-container">
           <img src={ukFlag} height="100" className="skill-icon" />
-          <p className="skill-caption p2">
+          <p className="skill-caption p2 language-caption">
             English (native)
           </p>
         </div>
         <div className="skill-container">
           <img src={portugalFlag} height="100" className="skill-icon" />
-          <p className="skill-caption p2">
+          <p className="skill-caption p2 language-caption">
             Portuguese (limited proficiency)
           </p>
         </div>
