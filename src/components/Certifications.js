08459bb04cdf10e7e6683a3ea8e8c6f1e2df8491
John Davies <jd.other@hotmail.com>
2022-11-27 20:31:52 +0000

    removed full stop

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -12,7 +12,7 @@ function Certifications() {
         <img className="certificate-image" src={awsDevBadge}></img>
         {/* may need to remove these unecessary classes - see the css too */}
         <div className="certificate-caption ms-4">
-          <p className="p1 certificate-title">AWS Developer Associate (DVA-C01).</p>
+          <p className="p1 certificate-title">AWS Developer Associate (DVA-C01)</p>
           <p className="p1">Awarded 10/2022. Expires 10/2025.</p>
           <a href="https://www.credly.com/badges/fcdb23d1-c292-411e-8b44-c43bc1edaf2e/public_url" className="icon-link" target="_blank" rel="noreferrer"><i className="bi bi-box-arrow-up-right"></i></a>
         </div>
