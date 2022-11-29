cab2337e193d834824ad8850e59f7fac52924543
John Davies <jd.other@hotmail.com>
2022-11-29 21:22:40 +0000

    updated bio

--- src/components/content/Bio.js
+++ src/components/content/Bio.js
@@ -2,7 +2,7 @@ function Bio() {
   return (
     <>
       <p className="p1">
-        Hi! My name is John. I am a dynamic and voracious learner who is always looking for a subject to learn or a project to build. Having a naturally logical, abstract and analytical way of reasoning is what drew me to software development. I hold the AWS developer associate certification, and I am currently studying for the certified Terraform associate exam. 
+        Hi! My name is John. I am a dynamic and voracious learner who is always looking for a subject to learn or a project to build. Having a naturally logical, abstract and analytical way of reasoning is what drew me to software development. I hold certifications in both AWS and Terraform. 
       </p>
       <p className="p1">
         Since joining the industry as a professional software engineer in April 2022, I have had the pleasure of applying my expertise at both a startup and an established software company. During this time, I have gained a wealth of knowledge surrounding the software development lifecycle and process, most notably in cloud computing and Amazon Web Services. I am now actively seeking a role as an AWS cloud engineer.
