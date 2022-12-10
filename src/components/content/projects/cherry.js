60995dff15f36c7c8a17f501872487c5afdd774c
John Davies <jd.other@hotmail.com>
2022-12-10 16:33:57 +0000

    updated cherry project wording

--- src/components/content/projects/cherry.js
+++ src/components/content/projects/cherry.js
@@ -1,6 +1,6 @@
 const data = {
   'title': 'Cherry - Bitcoin Block Explorer',
-  'content': 'Cherry is a block explorer built with serverless AWS services like Lambda, API Gateway and Lambda. It allows users to search for real-time and historical information about the Bitcoin blockchain, including data related to blocks, transactions and addresses.',
+  'content': 'Cherry is a block explorer built with serverless AWS services like Lambda, API Gateway and S3. It allows users to search for real-time and historical information about the Bitcoin blockchain, including data related to blocks, transactions and addresses.',
   'github': 'https://github.com/tigeryant/cherry-fe-v1',
   'live': 'http://cherry-frontend.s3-website.eu-west-2.amazonaws.com/',
   'technologies': [
