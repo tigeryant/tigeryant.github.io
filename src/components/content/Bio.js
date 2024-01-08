4af3f1262f7797a3341fe623ca93362f2d71b83b
John Davies <jd.other@hotmail.com>
2024-01-09 02:34:19 +0700

    updated bio, added bullet points

--- src/components/content/Bio.js
+++ src/components/content/Bio.js
@@ -1,23 +1,40 @@
 function Bio() {
   return (
-    <>
-      <p className="p1">
-        Proactive and ambitious engineer with a track record substantiated by performant and scalable software for cloud based, cryptographic and financial applications. Perpetually driven to learn and apply new knowledge. Certified in both AWS and Terraform. Engaged and enthusiastic member of the broader software development community, with regular attendances at hackathons, meetups and conferences. Interests include mathematics, Bitcoin and finance.
-      </p>
-      {/* <p className="p1">
-        Since joining the industry as a professional software engineer in April 2022, I have had the pleasure of applying my expertise at both a startup and an established software company. During this time, I have gained a wealth of knowledge surrounding the software development lifecycle and process, most notably in cloud computing and Amazon Web Services. I am now actively seeking a role as an AWS cloud engineer.
-      </p> */}
-      {/* Hi! My name is John. I am a dynamic and voracious learner who is always looking for a subject to learn or a project to build. Having a naturally logical, abstract and analytical way of reasoning is what drew me to software development. I hold certifications in both AWS and Terraform.  */}
-      {/* <p className="p1">
-Hi! My name is John. I am a dynamic and voracious learner who is always looking for a subject to learn or a project to build. Having a naturally logical, abstract and analytical way of reasoning is what drew me to software development. I hold the AWS developer associate certification, and I am currently studying for the certified Terraform associate exam.
-
-Since joining the industry as a professional software engineer in April 2022, I have had the pleasure of applying my expertise at both a startup and an established software company. During this time, I have gained a wealth of knowledge surrounding the software development lifecycle and process, most notably in cloud computing and Amazon Web Services. I am now actively seeking a role as an AWS cloud engineer.
-      </p> */}
-      {/* <p className="p1">
-        Some of my other interests include mathematics, cryptography, Bitcoin and finance.
-      </p> */}
-    </>
-  )
+    <ul style={{ listStyleType: "disc" }}>
+      <li className="p1" >
+        Driven engineer dedicated to architecting innovative, high-performance
+        cloud-native solutions, propelled by a passion for technological
+        advancement and problem-solving.
+      </li>
+      <li className="p1">
+        Spearheaded the successful implementation of a scalable,
+        cost-effective, serverless AWS solution in my current role at Gebler
+        Tooth Architects, complementing a robust portfolio of diverse AWS
+        projects.
+      </li>
+      <li className="p1">
+        Dedicated to staying ahead in the rapidly evolving tech
+        landscape, as showcased by my attainment of certifications in both AWS
+        and Terraform.
+      </li>
+      <li className="p1">
+        Engaged and enthusiastic member of the broader software
+        development community, actively contributing by volunteering at
+        conferences, in addition to regular participation at hackathons and
+        meetups.
+      </li>
+      <li className="p1">
+        Intrigued by the intersection of diverse disciplines that
+        amplify my expertise - my interests include fitness, linguistics and
+        Bitcoin.
+      </li>
+      <li className="p1">
+        Seeking opportunities to collaborate on innovative projects
+        that leverage technology to solve real-world problems and drive
+        meaningful change.
+      </li>
+    </ul>
+  );
 }
 
-export default Bio
+export default Bio;
